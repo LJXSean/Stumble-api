3dddec98f6a79b0ba179c40113de6671e06fba55
Sean <sean250.sl@gmail.com>
2023-06-11 15:40:37 +0800

    Register user to db

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,10 @@
 // Post requests to API/Users
 const express = require('express');
 const router = express.Router();
-const { check, validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator'); 
+const User = require('../../models/User');
+const gravatar = require('gravatar');
+const bcrypt = require('bcryptjs');
 
 // comment format: <REQ TYPE> <ENDPOINT>
 
@@ -14,12 +17,50 @@ router.post('/',
         check('email', 'Please include a valid Email').isEmail(),
         check('password', 'Please enter a valid password with 6 or more characters').isLength({ min: 6})
     ],
-    (req, res) => {
+    async (req, res) => {
         const errors = validationResult(req);
         if (!errors.isEmpty()) {
             return res.status(400).json({ errors: errors.array() });
         }
-        res.send('User route')
+
+        const { name, email, password } = req.body;
+
+        try {
+            // See if user exists (findOne document matching condition of email = email)
+            let user = await User.findOne({ email });
+
+            if (user) {
+                // Format error message to match validation errors above
+                return res.status(400).json({ error: [{"msg": "User already exists"}]});
+            }
+
+            // Get user's gravatar
+            const avatar = gravatar.url(email, {
+                s: '200',
+                r: 'pg',
+                d: 'monsterid'
+            });
+            
+            // Creates model instance (not saved yet)
+            user = new User({
+                name,
+                email,
+                avatar,
+                password
+            });
+
+            // Encrypt password 
+            const salt = await bcrypt.genSalt(10);
+            user.password = await bcrypt.hash(password, salt);
+            await user.save();
+
+            // Return jsonwebtoken -> To log in user immediately after signing up for account
+            res.send('User registered')
+        } catch(err) {
+            console.error(err.message);
+            res.status(500).send('Server Error');
+        }
+
 });
 
 module.exports = router; 
