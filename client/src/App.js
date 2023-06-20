5d5dee73e78ad0c86af798a6defa72609233154d
Sean <sean250.sl@gmail.com>
2023-06-21 01:22:33 +0800

    Create route handler and register form

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
 import './App.css';
 
 const App = () => (
-  <>
-    <Navbar />
-    <Landing />
-  </>
+  <Router>
+    <>
+      <Navbar />
+      <Routes>
+        <Route path='/' element={<Landing />} />
+        <Route path='register' element={<Register />} />
+        <Route path='login' element={<Login />} />
+        {/* <Route path='profiles' element={<Profiles />} />
+        <Route path='profile/:id' element={<Profile />} /> */}
+      </Routes>
+    </>
+  </Router>
 );
 
 export default App;
