82ff8950487707007b344dd552cbab2b91c59f31
Sean <sean250.sl@gmail.com>
2023-07-03 14:32:09 +0800

    Profile experience and education display

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -19,6 +19,7 @@ import {
 
 // Get current user's profile
 export const getCurrentProfile = () => async (dispatch) => {
+  dispatch({ type: CLEAR_PROFILE });
   try {
     const res = await axios.get('/api/profile/me');
 
