ed78991c71e82d7be8e0154220ef365a68677c11
Sean <risingsun221@gmail.com>
2023-07-13 22:09:13 +0800

    Prepare for production

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const connectDB = require('./config/db');
+const dotenv = require('dotenv');
+dotenv.config();
 const app = express();
+const path = require('path');
 
 // Connect Database
 connectDB();
@@ -8,16 +11,24 @@ connectDB();
 // Init middleware (for req.body parser in user api)
 app.use(express.json({ extended: false }));
 
-app.get('/', (req, res) => res.send(`API RUNNING based on ${req.url}`))
-
-const PORT = process.env.PORT || 5000;
-
 // Define Routes
 app.use('/api/users', require('./routes/api/users'));
 app.use('/api/auth', require('./routes/api/auth'));
 app.use('/api/posts', require('./routes/api/posts'));
 app.use('/api/profile', require('./routes/api/profile'));
 
+// Serve static assets in production
+if (process.env.NODE_ENV == 'production') {
+  // set static folder
+  app.use(express.static(path.join(__dirname, 'client', 'build')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const PORT = process.env.PORT || 5000;
+
 app.listen(PORT, () => {
-    console.log('running')
+  console.log(`Server in ${process.env.NODE_ENV} mode, listening on :${PORT}`);
 });
