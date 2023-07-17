70d9096f8df81639460759cb3d5650361977914d
Sean <risingsun221@gmail.com>
2023-07-17 23:45:35 +0800

    Sidebar and main layout done

--- client/src/components/newprofile/Profile.js
+++ client/src/components/newprofile/Profile.js
@@ -2,12 +2,10 @@ import React, { useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
-import ProfileExperience from './ProfileExperience';
-import ProfileEducation from './ProfileEducation';
-import ProfileGithub from './ProfileGithub';
 import { getProfileById } from '../../actions/profile';
 import { Link, useParams } from 'react-router-dom';
 import './profile.css';
+import ProfileSidebar from './ProfileSidebar';
 
 // props.match used to get the params from the URL
 const Profile = ({ getProfileById, profile: { profile, isLoading }, auth }) => {
@@ -32,41 +30,13 @@ const Profile = ({ getProfileById, profile: { profile, isLoading }, auth }) => {
                 Edit Profile
               </Link>
             )}
-          <div className='profile-grid my-1'>
-            {' '}
-            <div className='profile-exp bg-white p-2'>
-              <h2 className='text-primary'>Experience</h2>
-              {profile.experience.length > 0 ? (
-                <>
-                  {profile.experience.map((experience) => (
-                    <ProfileExperience
-                      key={experience._id}
-                      experience={experience}
-                    ></ProfileExperience>
-                  ))}
-                </>
-              ) : (
-                <h4>No experience credentials</h4>
-              )}
+          <div className='carousel'>Side Project Carousel</div>
+          <div className='layout'>
+            <ProfileSidebar profile={profile} />
+            <div className='layout-main' style={{ backgroundColor: 'black' }}>
+              <div className='projects'>Projects</div>
+              <div className='bottom'>Experience, Education</div>
             </div>
-            <div className='profile-edu bg-white p-2'>
-              <h2 className='text-primary'>Education</h2>
-              {profile.education.length > 0 ? (
-                <>
-                  {profile.education.map((education) => (
-                    <ProfileEducation
-                      key={education._id}
-                      education={education}
-                    ></ProfileEducation>
-                  ))}
-                </>
-              ) : (
-                <h4>No experience credentials</h4>
-              )}
-            </div>
-            {profile.githubusername && (
-              <ProfileGithub username={profile.githubusername} />
-            )}
           </div>
         </>
       )}
---
+++ client/src/components/newprofile/ProjectCards.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const ProjectCards = (props) => {
+  return <div style={{ backgroundColor: 'black' }}>ProjectCards</div>;
+};
+
+ProjectCards.propTypes = {};
+
+export default ProjectCards;
