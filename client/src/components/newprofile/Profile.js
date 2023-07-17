d3f22d7af82791ce0d86130792af26df1f0c1aed
Sean <risingsun221@gmail.com>
2023-07-17 22:24:12 +0800

    Create template files

---
+++ client/src/components/newprofile/Profile.js
@@ -0,0 +1,87 @@
+import React, { useEffect } from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Spinner from '../layout/Spinner';
+import ProfileExperience from './ProfileExperience';
+import ProfileEducation from './ProfileEducation';
+import ProfileGithub from './ProfileGithub';
+import { getProfileById } from '../../actions/profile';
+import { Link, useParams } from 'react-router-dom';
+import './profile.css';
+
+// props.match used to get the params from the URL
+const Profile = ({ getProfileById, profile: { profile, isLoading }, auth }) => {
+  const { id } = useParams();
+  useEffect(() => {
+    getProfileById(id);
+  }, [getProfileById, id]);
+
+  return (
+    <section className='profile-container'>
+      {profile === null || isLoading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Link to='/profiles' className='btn btn-light'>
+            Back to Profiles
+          </Link>
+          {auth.isAuthenticated &&
+            auth.isLoading === false &&
+            auth.user._id === profile.user._id && (
+              <Link to='/edit-profile' className='btn btn-dark'>
+                Edit Profile
+              </Link>
+            )}
+          <div className='profile-grid my-1'>
+            {' '}
+            <div className='profile-exp bg-white p-2'>
+              <h2 className='text-primary'>Experience</h2>
+              {profile.experience.length > 0 ? (
+                <>
+                  {profile.experience.map((experience) => (
+                    <ProfileExperience
+                      key={experience._id}
+                      experience={experience}
+                    ></ProfileExperience>
+                  ))}
+                </>
+              ) : (
+                <h4>No experience credentials</h4>
+              )}
+            </div>
+            <div className='profile-edu bg-white p-2'>
+              <h2 className='text-primary'>Education</h2>
+              {profile.education.length > 0 ? (
+                <>
+                  {profile.education.map((education) => (
+                    <ProfileEducation
+                      key={education._id}
+                      education={education}
+                    ></ProfileEducation>
+                  ))}
+                </>
+              ) : (
+                <h4>No experience credentials</h4>
+              )}
+            </div>
+            {profile.githubusername && (
+              <ProfileGithub username={profile.githubusername} />
+            )}
+          </div>
+        </>
+      )}
+    </section>
+  );
+};
+
+Profile.propTypes = {
+  getProfileById: PropTypes.func.isRequired,
+  profile: PropTypes.object.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  profile: state.profile,
+  auth: state.auth,
+});
+export default connect(mapStateToProps, { getProfileById })(Profile);
