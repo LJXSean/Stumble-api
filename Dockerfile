29307721bd728a4442d6179bc4e7c6c8aa1a113d
LJXSean <risingsun221@gmail.com>
2023-08-27 17:17:01 +0800

    removed client folder to dockerise backend only

---
+++ Dockerfile
@@ -0,0 +1,19 @@
+# Define the image you want to build from.
+FROM node:14
+
+# Create app directory to hold application code inside the image
+WORKDIR /usr/src/app
+# The image comes with Node.js and NPM already installed.
+# We just need to install the rest of our dependencies.
+# Copy package.json and package-lock.json to the app directory on the image.
+COPY package*.json ./
+# Install dependencies.
+RUN npm install
+# Uncomment the following line if you are building code for production.
+# RUN npm ci --omit=dev
+# Bundle the app's source code inside the Docker image.
+COPY . .
+# Expose port 8080 so it can be mapped by Docker daemon.
+EXPOSE 5000
+# Define the command to run your app using CMD which defines your runtime.
+CMD [ "node", "server.js" ]
--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const connectDB = require('./config/db');
-const dotenv = require('dotenv');
+const express = require("express");
+const connectDB = require("./config/db");
+const dotenv = require("dotenv");
 dotenv.config();
 const app = express();
-const path = require('path');
+const path = require("path");
 
 // Connect Database
 connectDB();
@@ -12,20 +12,10 @@ connectDB();
 app.use(express.json({ extended: false }));
 
 // Define Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/posts', require('./routes/api/posts'));
-app.use('/api/profile', require('./routes/api/profile'));
-
-// Serve static assets in production
-if (process.env.NODE_ENV == 'production') {
-  // set static folder
-  app.use(express.static(path.join(__dirname, 'client', 'build')));
-
-  app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-  });
-}
+app.use("/api/users", require("./routes/api/users"));
+app.use("/api/auth", require("./routes/api/auth"));
+app.use("/api/posts", require("./routes/api/posts"));
+app.use("/api/profile", require("./routes/api/profile"));
 
 const PORT = process.env.PORT || 5000;
 
